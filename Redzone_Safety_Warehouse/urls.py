2319b86194ba541e2f44c2d8934cdc428d1f9faa
Moyzes Campos <moyzesstaron@gmail.com>
2025-05-04 16:53:24 -0300

    Arrumando o erro de digitacao

--- Redzone_Safety_Warehouse/urls.py
+++ Redzone_Safety_Warehouse/urls.py
@@ -23,4 +23,4 @@ from django.contrib import admin
 urlpatterns = [
     path('api/v1', include('core.urls')),
     path('admin/', admin.site.urls)
-] + static(settings.MEDIA_gURL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
