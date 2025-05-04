17cd8985bd440e01beaf1395216113718adb5969
Moyzes Campos <moyzesstaron@gmail.com>
2025-05-04 16:49:16 -0300

    Adicionando ao Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Video
+from .tasks import process_video
 
-# Register your models here.
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'file', 'processed', 'created_at')
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        # dispara o processamento se for um upload novo (não somente edição)
+        if not change:
+            process_video.delay(obj.id)
